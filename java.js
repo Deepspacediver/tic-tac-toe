d17183dd9bd7139cc125b8f44a78e477858f1993
deepspacediver <luka.koscik@gmail.com>
2022-07-23 19:51:47 +0200

    added function to display array in tiles, created players

--- java.js
+++ java.js
@@ -1,6 +1,40 @@
 console.log("hello world")
 
 const game = (() => {
-    const gameBoard = [];
-    console.log(gameBoard)
+    const gameBoard = ['x', 'o', 'x',];
+    const gameTiles = Array.from(document.querySelectorAll('div.game-tile'))
+
+    const displayGameBoardArray = () =>{
+        for (let i = 0; i < gameBoard.length; i++) {
+                gameTiles[i].innerText = gameBoard[i]
+        }
+    }
+    
+    
+    displayGameBoardArray()
+    return{
+        gameTiles,
+        gameBoard,
+    }
 })();
+
+const Player = (sign) => {
+    const playingSign = sign;
+    const playerMove = (sign) =>{
+        game.gameTiles.forEach(tile => {
+            tile.addEventListener('click', (e) => {
+                e.target.innerText = sign
+                e.target.dataset.player = `${sign}`
+                game.gameBoard.push(sign)
+            })
+            
+        })
+    }
+
+    playerMove(playingSign)
+    return {
+        playingSign
+    }
+}
+
+const player1 = Player('x')
