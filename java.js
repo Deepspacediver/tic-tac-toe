a1dbd6ffc1f326896ce9094c7cd349248715d828
deepspacediver <luka.koscik@gmail.com>
2022-07-25 19:35:43 +0200

    added ability to add marks and function that checks players turn

--- java.js
+++ java.js
@@ -1,40 +1,101 @@
 console.log("hello world")
 
-const game = (() => {
-    const gameBoard = ['x', 'o', 'x',];
-    const gameTiles = Array.from(document.querySelectorAll('div.game-tile'))
-
-    const displayGameBoardArray = () =>{
-        for (let i = 0; i < gameBoard.length; i++) {
-                gameTiles[i].innerText = gameBoard[i]
-        }
-    }
-    
-    
-    displayGameBoardArray()
-    return{
-        gameTiles,
-        gameBoard,
-    }
-})();
-
 const Player = (sign) => {
     const playingSign = sign;
-    const playerMove = (sign) =>{
-        game.gameTiles.forEach(tile => {
-            tile.addEventListener('click', (e) => {
-                e.target.innerText = sign
-                e.target.dataset.player = `${sign}`
-                game.gameBoard.push(sign)
-            })
-            
-        })
-    }
-
-    playerMove(playingSign)
     return {
-        playingSign
+        playingSign,
     }
 }
 
-const player1 = Player('x')
+const playerX= Player('X')
+const playerO = Player('O')
+
+
+const gameBoard = (() => {
+    const gameArray = [];
+    const gameTiles = Array.from(document.querySelectorAll('div.game-tile'))
+    
+    
+   
+
+    /* const displayGameBoardArray = () =>{
+        for (let i = 0; i < gameArray.length; i++) {
+                gameTiles[i].innerText = gameArray[i]
+        }
+    } */
+    const assignIndex = () => {
+        for (let i = 0; i < gameTiles.length; i++) {
+            gameTiles[i].dataset.index = `${i}`;
+            
+        }
+    }
+    const gameBoardEvents = (xSign, oSign) =>{
+        gameTiles.forEach(tile => {
+            tile.addEventListener('click', (e)=>{
+                let playerTurn = gameLogic.checkPlayerTurn()
+                if(!!e.target.innerText) return;
+                if(playerTurn === 'X') {
+                    e.target.innerText =`${playerTurn}`
+                    gameBoard.gameArray[e.target.dataset.index]=`${playerTurn}`
+
+                    console.log(gameBoard.gameArray)
+                }
+                else if(playerTurn === 'O'){
+                    e.target.innerText = `${playerTurn}`
+                    gameBoard.gameArray[e.target.dataset.index]=`${playerTurn}`
+                    console.log(gameBoard.gameArray)
+                };
+            })
+        })
+    }
+    assignIndex()
+    gameBoardEvents()
+    return{
+        gameTiles,
+        gameArray,
+    }
+})();
+
+const gameLogic = (() =>{
+    const checkPlayerTurn = () =>{
+        let playerTurn;
+        let amountOfX = gameBoard.gameArray.filter(i => i==='X').length;
+        let amountOfO = gameBoard.gameArray.filter(i => i==='O').length;
+        if(!gameBoard.gameArray || amountOfX <= amountOfO) return playerTurn = 'X';
+        else if(amountOfX > amountOfO) return playerTurn ='O';
+    }
+    return{
+        checkPlayerTurn,
+    }
+})();
+
+/*
+ const playGame = (player1, target) =>{
+    gameTiles.forEach(tile => {
+        tile.addEventListener('click', (e) =>{
+            let target = e.target;
+            player1.playerMove(player1.playingSign)
+        })
+    })
+    
+    if(!gameBoard.length) {
+        
+    }
+}
+const playerMove = (sign, target) =>{
+        if(!!target.innerText) return;
+        target.innerText = sign
+        target.dataset.player = `${sign}`
+        game.gameBoard.push(sign) 
+    }  
+     const playerChoice = () =>{
+        const playerButtonContainer = document.querySelector('player-container')
+        const player1Button = document.querySelector('.player1')
+        const player2Button = document.querySelector('.player2')   
+
+        playerButtonContainer.addEventListener('click', ()=>{
+            let 
+        })
+    } 
+ */
+
--- readME.md
+++ readME.md
@@ -1,1 +1,13 @@
 Tic Tac Toe Game Project from Odin curriculum 
+
+
+Logic of the Game :
+
+Game Start
+    if array is empty - let player choose a who is first
+        if playerX is chosen {tile innerText is X and X is pushed into Array}
+        else if playerO is chosen {innerTex tis O and O is pushed into array}
+Game next
+    if last Array item is X then playerO plays
+    if last Array item is O then playerX plays
+    
